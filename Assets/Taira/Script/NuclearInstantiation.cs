4015f0e99b048379dfe1043fe008d6c14a79b40a
LAPTOP-UEIRJFST\moyo0 <yuki-t-17d@stu.kbc.ac.jp>
2019-04-05 12:11:46 +0900

    燭台当たり判定、火をつける

---
+++ Assets/Taira/Script/NuclearInstantiation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NuclearInstantiation : MonoBehaviour {
+
+    public GameObject Nuclear;
+
+	// Use this for initialization
+	void Start() { 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        OnClick();
+
+    }
+
+    void OnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(Nuclear);
+        }
+    }
+}
