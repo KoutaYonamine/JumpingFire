e8784bba28b18fce1e8a376ce71b74f18cb891f7
moyo123 <yuki-t-17d@stu.kbc.ac.jp>
2019-04-17 10:59:45 +0900

    当たり判定の種類追加。

--- Assets/Taira/Script/ColliderLength.cs
+++ Assets/Taira/Script/ColliderLength.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class ColliderLength : MonoBehaviour {
 
-    Vector3 pos;
+    private Vector3 P_Position;
+    private Vector3 Difference;
+    private float Magnitude;
+    private bool LengthCheckFlg;
+
+    [SerializeField] private float Length;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log(pos = (GameObject.Find("Cube")).GetComponent<Renderer>().bounds.center);
 	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("あ");
+            P_Position = collision.transform.position; //プレイヤーの座標を代入
+            Difference = P_Position - transform.position;
+            Magnitude = Difference.magnitude;
+            Debug.Log(Magnitude);
+            LengthCheck();
+        }
+    }
+
+    private void LengthCheck()
+    {
+        if(Magnitude <= Length)
+        {
+            Debug.Log("当たり");
+        }
+        else if(Magnitude > Length)
+        {
+            Debug.Log("はずれ");
+        }
+    }
 }
