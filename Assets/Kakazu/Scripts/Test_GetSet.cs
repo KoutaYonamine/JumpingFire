42d041209749d726eb94fc09aa863c428dc5e899
moyo123 <yuki-t-17d@stu.kbc.ac.jp>
2019-04-17 12:00:45 +0900

    get.set追加

---
+++ Assets/Kakazu/Scripts/Test_GetSet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test_GetSet : MonoBehaviour {
+
+    private bool TestFlg = false;
+
+    public bool Test
+    {
+        get
+        {
+            return TestFlg;
+        }
+        set
+        {
+            TestFlg = value;
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(TestFlg);
+	}
+}
--- Assets/Taira/Script/ColliderLength.cs
+++ Assets/Taira/Script/ColliderLength.cs
@@ -4,43 +4,53 @@ using UnityEngine;
 
 public class ColliderLength : MonoBehaviour {
 
-    private Vector3 P_Position;
-    private Vector3 Difference;
-    private float Magnitude;
-    private bool LengthCheckFlg;
+    private Vector3 P_Position; //プレイヤーのポジション
+    private Vector3 Difference; //プレイヤーと燭台の差分
+    private float Magnitude; 
 
-    [SerializeField] private float Length;
+    private bool LengthCheckFlg; //範囲内だとtrue;
+    private bool Flg;
+
+    public GameObject Player;
+    private Test_GetSet Script;
+
+    [SerializeField] private float Length; //範囲(inspectorで変更可能)
 
 	// Use this for initialization
 	void Start () {
-
+        Script = Player.GetComponent<Test_GetSet>();
     }
 	
 	// Update is called once per frame
 	void Update () {
+        
 	}
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.tag == "Player")
+        if(collision.gameObject.tag == "Player") //プレイヤータグ？
         {
-            Debug.Log("あ");
             P_Position = collision.transform.position; //プレイヤーの座標を代入
-            Difference = P_Position - transform.position;
+            Difference = P_Position - transform.position; //差分
             Magnitude = Difference.magnitude;
             Debug.Log(Magnitude);
-            LengthCheck();
+            LengthCheck(); //フラグ切り替え
         }
     }
 
-    private void LengthCheck()
+    private void LengthCheck() //フラグ切り替え
     {
         if(Magnitude <= Length)
         {
+            Script.Test = true;
+            Debug.Log(Script.Test);
+
+            LengthCheckFlg = true; //当たり
             Debug.Log("当たり");
         }
         else if(Magnitude > Length)
         {
+            LengthCheckFlg = false; //はずれ
             Debug.Log("はずれ");
         }
     }
