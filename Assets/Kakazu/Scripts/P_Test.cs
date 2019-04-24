74e4bf2b74df4695d935271d0516137bc66d7a23
Chika_13 <kakazu1314@gmail.com>
2019-04-24 10:13:06 +0900

    まとめて初期化するscriptの作成 ※初期化するタイミングの処理がないため、実装はまだ

--- Assets/Kakazu/Scripts/P_Test.cs
+++ Assets/Kakazu/Scripts/P_Test.cs
@@ -2,43 +2,68 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CS_Player : InitializeVariable     //サブクラス
-{
+public class P_Test : InitializeVariable{
+
     private Rigidbody rigidBody;
+
+    //private int ClickFlg = 99;//クリックしているかどうか
+    private bool ReleasedFlg;//連打禁止 
+
+    private bool FirstVelocity = true;//一度だけ入る(1フレーム目
+
+    private bool AddSpeedFlg; //燭台の中心に当たったかどうか
+    private bool Initialize; //スピード初期化判定
+
+
+    private float Speed;//移動速度
 
     private float x, y, z;//プレイヤーの移動座標
 
-    private float FreeFallGrvity = 9.8f;//フレーム後に与える力
-    private float UnnaturalGrvity = 19.6f;//指を離した時に与える力
+    private Vector3 Force;//AddForce
+    private float Force_y;//yに与える力
+    private float FreeFallGrvity;//フレーム後に与える力
+    private float UnnaturalGrvity;//指を離した時に与える力
+    private int FrameCount;//フレームをカウント
 
     private Vector3 StartPosition;//初期位置
-    private GameObject Camera;//カメラをゲットコンポーネント
+    public GameObject Camera;//カメラをゲットコンポーネント
     private Vector3 CameraPosition;//カメラのポジション
 
     private float Length;//半径
     float AtanAngle;//方位角　角度
     float count;
 
+    [SerializeField] private float RotateSpeed;//円運動の速度
+    [SerializeField] private Vector3 Vel;//初速度
+
+    // Use this for initialization
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
 
         StartPosition = this.transform.position;
-        Camera = GameObject.Find("Main Camera");
         CameraPosition = Camera.transform.position;
+
+        Speed = 1.0f;
+        Force_y = 20.0f;
+
+        FreeFallGrvity = 9.8f;
+        UnnaturalGrvity = 19.6f;
+
+        FrameCount = 0;
 
         Length = transform.position.magnitude - 0.5f;
         AtanAngle = Mathf.Atan2(StartPosition.x, StartPosition.z);
         count = AtanAngle;
-        
+
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         InputMouse_Touch();
 
-        if(ClickFlg == 2)
+        if (ClickFlg == 2)
             FrameCount++;
     }
     private void FixedUpdate()
@@ -62,12 +87,9 @@ public class CS_Player : InitializeVariable     //サブクラス
                     ReleasedFlg = false;
                 }
             }
-        }
-        else
-        {
+        } else {
             // タッチされているかチェック
-            if (Input.touchCount > 0)
-            {
+            if (Input.touchCount > 0) {
                 // タッチ情報の取得
                 Touch touch = Input.GetTouch(0);
 
@@ -95,7 +117,7 @@ public class CS_Player : InitializeVariable     //サブクラス
     {
         if (ClickFlg == 0) {
             count += Time.deltaTime * RotateSpeed;
-　
+
             CircularMotion();//円運動
 
             Force_y = Force_y - UnnaturalGrvity;//離した時に急な落下をさせる
@@ -144,11 +166,14 @@ public class CS_Player : InitializeVariable     //サブクラス
             FrameCount = 0;//フレームカウントを初期化
             ClickFlg = 99;
         }
+
+        if (collision.gameObject.tag == "Candle") {
+        }
     }
 
     public bool addspeed//スピードの変化
     {
-        get { return AddSpeedFlg ; }
+        get { return AddSpeedFlg; }
         set { AddSpeedFlg = value; }
     }
     public bool initialize//燭台に乗ったかどうか
@@ -156,7 +181,7 @@ public class CS_Player : InitializeVariable     //サブクラス
         get { return Initialize; }
         set { Initialize = value; }
     }
-  
+
     private void CircularMotion()//円運動
     {
         x = Length * Mathf.Sin(count);
